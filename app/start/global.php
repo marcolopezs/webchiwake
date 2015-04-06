915a696c4f8807b14dab4d74f7d9f96ec69eb163
Marco Lopez <mlopez@marostdevelopers.com>
2015-04-06 14:16:17 -0500

    nuevas funciones

--- app/start/global.php
+++ app/start/global.php
@@ -79,3 +79,76 @@ App::down(function()
 */
 
 require app_path().'/filters.php';
+
+
+
+//MOVER ARCHIVO
+function FileMove($file, $path)
+{
+    $name = $file->getClientOriginalName();					//NOMBRE Y EXTENSION DE ARCHIVO
+    $extension = $file->getClientOriginalExtension();		//EXTENSION DE ARCHIVO
+    $archive = basename($name, ".".$extension);			    //NOMBRE DE ARCHIVO
+    $pathName = Str::slug($archive);						//CONVERTIR NOMBRE SIN ESPACIOS
+    $filename = $pathName.".".$extension;					//NOMBRE Y EXTENSION SIN ESPACIOS
+    $file->move($path, $filename);							//MOVER ARCHIVO A NUEVA CARPETA
+    return $filename;
+}
+
+//CARPETA CON NOMBRE DEL MES ACTUAL
+function FechaCarpeta()
+{
+    $meses = array("enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre");
+    $mes = date('n')-1; // devuelve el número del mes
+    $ano = date('Y'); // devuelve el año
+    $fechaCarpeta = $meses[$mes]."".$ano."/";
+    return $fechaCarpeta;
+}
+
+//CREACION DE CARPETA
+function CrearCarpeta(){
+    $nombre_carpeta=fechaCarpeta();
+    $ruta = $_SERVER['DOCUMENT_ROOT']."upload/".$nombre_carpeta;
+    if(!is_dir($ruta)){
+        @mkdir($ruta, 0755);
+        $carpeta=$ruta;
+    }else{
+        $carpeta=$ruta;
+    }
+    return $carpeta;
+}
+
+//GENERAR CODIGO ALEATORIO
+function CodigoAleatorio($length=10,$uc=TRUE,$n=TRUE,$sc=FALSE)
+{
+    $source = 'abcdefghijklmnopqrstuvwxyz';
+    if($uc==1) $source .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if($n==1) $source .= '1234567890';
+    if($sc==1) $source .= '|@#~$%()=^*+[]{}-_';
+    if($length>0){
+        $rstr = "";
+        $source = str_split($source,1);
+        for($i=1; $i<=$length; $i++){
+            mt_srand((double)microtime() * 1000000);
+            $num = mt_rand(1,count($source));
+            $rstr .= $source[$num-1];
+        }
+    }
+    return $rstr;
+}
+
+//LISTA DE MENU PARA HOME
+function menuHome( $id_padre = 0 ){
+    $menu = '';
+    $query = Menu::orderBy('orden', 'asc')->where('parent_id', $id_padre)->get();
+    $cant = Menu::all()->count();
+    if( $cant > 0 ){ $menu .= '<ul class="dd-list">'; }
+    foreach ($query as $item){
+        $menu .=  '<li>';
+        $menu .= '<a class="round_left" href="'.$item->slug_url.'">';
+        $menu .= $item->titulo;
+        $menu .= '</a>';
+        $menu .= menuHome( $item->id ) . '</li>';
+    }
+    if( $cant > 0 ){ $menu .= '</ul>'; }
+    return $menu;
+}
